e54485abfe9ec30cb86d30ef937c5a24d2e78918
Embraccce <1767310416@qq.com>
2024-05-09 22:42:08 +0800

    更新1560简单做法，过百分之八十数据

--- 十二届国赛/1560.py
+++ 十二届国赛/1560.py
@@ -1,24 +1,16 @@
+from collections import Counter
 mod = 998244353
 n,m,k = map(int,input().split())
-arr = [0]+list(map(int,input().split()))
-d = {i: arr[i] for i in range(1, n+1)}
-del arr
+d = Counter(map(int, input().split()))
 for _ in range(m):
     X,Y = map(int,input().split())
-    for key,value in d.items():
-        d[key] += X
-    d = {key: value for key, value in d.items() if value > 0}
-    temp = dict()
-    idx = len(d)+1
-    for key,value in d.items():
-        if value > k:
-            d[key] = k
-            for i in range(value-k):
-                temp[idx] = 1
-                idx+=1
+    d = {key+X: val % mod for key, val in d.items() if key+X>0}
+    for key,val in d.copy().items():
+        if key > k:
+            del d[key]
+            d[1] = (d.get(1, 0) + (key - k) * val) % mod
+            d[k]= (d.get(k ,0) + val) % mod
     if Y != 0:
-        temp[idx] = Y
-    d.update(temp)
-    print(sum(d.values()) % mod)
+        d[Y] = (d.get(Y, 0) + 1) % mod
+    print(sum((key * value) % mod for key, value in d.items()) % mod)
 
-    
